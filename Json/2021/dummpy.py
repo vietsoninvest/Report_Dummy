639e250de5c290baacb8dce95b82a23f18c09893
vietsoninvest <sondv.ftu97@gmail.com>
2024-10-06 23:19:12 +1100

    addon

--- Json/2021/dummpy.py
+++ Json/2021/dummpy.py
@@ -13,15 +13,12 @@ def random_string(length=8):
 # dummy replacement
 for record in data:
     record['Order_ID'] = random.randint(1000, 9999) 
-    record['Date'] = datetime.now().strftime('%Y-%m-%d') 
-    record['Time'] = datetime.now().strftime('%H:%M:%S')  
     record['Outlet'] = random.choice(outlets)  
     record['Model'] = random_string()  #
-    record['ProductID'] = random.randint(100, 500)  #
     record['ProductName'] = random_string(12)  
     record['Price'] = round(random.uniform(50, 500), 2) 
-    record['Revenue'] = round(random.uniform(15000, 100000), 2)  
-    record['Profit'] = round(random.uniform(15000, 100000), 2)
+    record['Revenue'] = round(random.uniform(15, 375), 2)  
+    record['Profit'] = round(random.uniform(150, 200), 2)
 
 # Save the dummy data back to JSON
 with open('Json/2021/dummy_data_month_12.json', 'w') as f:
